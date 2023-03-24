9fb1930dc24d74111b721279e4eeaebd7b0ecf3b
gift56 <efeasiughu@gmail.com>
2023-03-24 07:52:19 -0700

    fixed navbar

--- type.js
+++ type.js
@@ -1,10 +1,23 @@
-function myFunction () {
-    let x = document.getElementById("nav-menu");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-}
+let menu = document.getElementById("nav-menu");
+const hamburgerIcon = document.getElementById("nav_bar");
+const closeIcon = document.getElementById("close");
 
-myFunction( );
+// Open menu when hamburger icon is clicked
+hamburgerIcon.addEventListener("click", () => {
+  menu.classList.add("display");
+  header.style.overflow = "revert";
+});
+
+// Close menu when close icon is clicked
+closeIcon.addEventListener("click", () => menu.classList.remove("display"));
+
+document.addEventListener("click", (event) => {
+  // Check if the clicked element is not inside the menu
+  if (!menu.contains(event.target) && !hamburgerIcon.contains(event.target)) {
+    menu.classList.remove("display");
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  header.style.overflow = "hidden";
+});
