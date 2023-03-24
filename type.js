c1ec7cbc6eeb0b6970ff14e6c912d2afc0e5563f
XerXerS <darkxerxers29@gmail.com>
2023-03-24 12:41:00 +0100

    css code

---
+++ type.js
@@ -0,0 +1,10 @@
+function myFunction () {
+    let x = document.getElementById("nav-menu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+}
+
+myFunction( );
